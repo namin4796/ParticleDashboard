1a0d681ce933015b2c6ce09f85d5957acc3e4e16
Neeraj Amin <neeraj.amin94@gmail.com>
2026-01-27 16:28:54 +0100

    fix: implement session-based CSV logging with save/discard dialog

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,12 @@
 import sys
+import json
 import csv
+import os
+import random
 from datetime import datetime
 from collections import deque
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QSlider, QCheckBox)
-
+from PyQt6.QtWidgets import (QMainWindow, QLabel, QVBoxLayout, QWidget, QPushButton, QSlider, QCheckBox, QMessageBox)
 import pyqtgraph as pg
 import pyqtgraph.exporters
 from worker import SensorWorker
@@ -14,8 +16,8 @@ class ParticleDashboard(QMainWindow):
         super().__init__()
 
         # 1. Window Setup
-        self.setWindowTitle("Particle Flux Monitor - v1.0")
-        self.resize(900, 600)
+        self.setWindowTitle("PySensorFlow Dynamic Dashboard")
+        self.resize(1000, 700)
 
         # --- GLOBAL STYLESHEET ---
         self.setStyleSheet("""
@@ -38,6 +40,8 @@ class ParticleDashboard(QMainWindow):
             }
         """)
 
+        # 1a Load config
+        self.load_config("config/setup.json")
     
         # 2. The Central Layout (The "Container")
         self.central_widget = QWidget()
@@ -57,8 +61,7 @@ class ParticleDashboard(QMainWindow):
         self.layout.addWidget(self.slider_label)
 
         self.threshold_slider = QSlider(Qt.Orientation.Horizontal)
-        self.threshold_slider.setMinimum(0)
-        self.threshold_slider.setMaximum(1023)
+        self.threshold_slider.setRange(0, 1023)
         self.threshold_slider.setValue(500)
 
         self.threshold_slider.valueChanged.connect(self.update_slider_label)
@@ -71,19 +74,37 @@ class ParticleDashboard(QMainWindow):
         self.layout.addWidget(self.chk_hw_sync)
 
         self.led_is_on = False
-        # plot widget
+        # dynamic plot widget
         self.graph_widget = pg.PlotWidget()
         self.graph_widget.setBackground('#1e1e1e')
-        self.graph_widget.setTitle("LDR Output", color="#bdc3c7", size="12pt")
-
+        self.graph_widget.setTitle("Real-Time Sensor Data", color="#bdc3c7", size="12pt")
         styles = {'color': '#bdc3c7', 'font-size': '12px'}
         self.graph_widget.setLabel('left', 'Raw Signal', units='0-1023', **styles)
         self.graph_widget.setLabel('bottom', 'Time Sample', **styles)
         self.graph_widget.showGrid(x=True, y=True, alpha=0.3)
-
-        self.data_buffer =[]
-        self.data_line = self.graph_widget.plot([], [], pen=pg.mkPen(color='#00e5ff', width=2))
+        self.graph_widget.addLegend()
         self.layout.addWidget(self.graph_widget)
+
+        # initialize plots dynamically
+        #storing plot items in dictionary
+        self.buffers = {}
+        self.curves = {}
+
+
+        #define a palette of colors for various sensors
+        colors = ['#00e5ff', '#ff5252', '#f1c40f', '#9b59b6', '#2ecc71']
+
+        for i, sensor in enumerate(self.sensor_config):
+            s_id = sensor['id']
+            s_name = sensor['name']
+
+            #create a bufferfor each 'i'th sensor
+            self.buffers[s_id] = deque(maxlen=100)
+
+            #create a curve for this sensor
+            color = colors[i % len(colors)]
+            curve = self.graph_widget.plot(pen=pg.mkPen(color=color, width=2), name=s_name)
+            self.curves[s_id] = curve
 
 
         # Create a control Button
@@ -103,6 +124,7 @@ class ParticleDashboard(QMainWindow):
         self.layout.addWidget(self.chk_smooth)
 
         self.smoothing_buffer = deque(maxlen=10)
+        self.smoothing_buffer_temp = deque(maxlen=10)
 
         # save a screenshot button
         self.btn_screenshot = QPushButton("SAVE SNAP")
@@ -115,61 +137,116 @@ class ParticleDashboard(QMainWindow):
         self.chk_record.setStyleSheet("color #aaaaaa;")
         self.layout.addWidget(self.chk_record)
 
+        self.dlg = QMessageBox(self)
+        self.dlg.setText("Do you want to save the data?")
+        self.dlg.setStandardButtons(
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+        )
+        self.dlg.setIcon(QMessageBox.Icon.Question)
+
         #log file
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         self.csv_file = None
         self.csv_write = None
+        self.filename = ""
 
-    # Logic functions
+        if not os.path.exists("data"):
+            os.makedirs("data")
+
+    def load_config(self, path):
+        try:
+            with open(path, 'r') as f:
+               config = json.load(f)
+               self.sensor_config = config.get('sensors', [])
+        except Exception as e:
+            print(f"Error loading config: {e}")
+            self.sensor_config = []
+
+# Logic functions
     
     def toggle_acquisition(self):
-        #check if button is pressed or released
 
+        #if self.chk_record.isChecked():
+
+        #check if button is pressed or released 
         if self.btn_start.isChecked():
-           arduino_port = "/dev/cu.usbmodem14301"
-           self.worker = SensorWorker(arduino_port)
-           self.worker.data_signal.connect(self.update_display)
-           
-           self.btn_start.setText("HALT ACQUISITION")
-           self.label.setText(">>> ACQUIRING DATA <<<")
-           self.label.setStyleSheet("color: #00e5ff; font-size: 24px; font-weight: bold;")
 
-           #create a unique filename based on current time
-           timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-           filename = f"data/particle_log_{timestamp}.csv"
+            if self.chk_record.isChecked():
+                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                self.filename = f"data/particle_log_{timestamp}.csv"
+                try:
+                    self.csv_file = open(self.filename, mode='w', newline='')
+                    self.csv_write = csv.writer(self.csv_file)
+                    self.csv_write.writerow(["Timestamp", "Flux_Value"])
+                    print(f"Started recording to: {self.filename}")
+                except Exception as e:
+                    print(f"Error creating file: {e}")
+            else:
+                self.csv_file = None
+                self.csv_write = None
+                
+            arduino_port = "/dev/cu.usbmodem14301"
+            self.worker = SensorWorker(arduino_port, config_path="config/setup.json")
+            self.worker.data_signal.connect(self.update_display)
+            
+            self.btn_start.setText("HALT ACQUISITION")
+            self.label.setText(">>> ACQUIRING DATA <<<")
+            self.label.setStyleSheet("color: #00e5ff; font-size: 24px; font-weight: bold;")
 
-           #if self.chk_record.isChecked():
-           self.csv_file = open(filename, mode='w', newline='')
-           self.csv_write = csv.writer(self.csv_file)
-
-           self.csv_write.writerow(["Timestamp", "Flux_Value"])
-           print(f"DEBUG: Recording to {filename}")
-           
-
-           self.worker.start()
+            self.worker.start()
 
         else:
             self.btn_start.setText("INITIALIZE ACQUISITION")
             self.label.setText("SYSTEM READY")
             self.label.setStyleSheet("color: #ecf0f1; font-size: 24px;")
-            self.worker.stop()
+         #   self.worker.stop()
 
             if hasattr(self, 'worker'):
                 self.worker.stop()
                 self.worker.wait()
 
-            #if self.csv_file:
-            #    self.csv_file.close()
-            #    self.csv_file = None
-            #    print("DEBUG: File closed safely.")
+            if self.csv_file:
+                self.csv_file.close()
+
+                self.button = self.dlg.exec()
+
+                if self.button == QMessageBox.StandardButton.Yes:
+                    print(f"Recording to {self.filename}")
+                elif self.button == QMessageBox.StandardButton.No:
+                    print(f"File {self.filename} is deleted")
+                    path = os.path.join(os.getcwd(), self.filename)
+                    if os.path.exists(path):
+                        try:
+                            os.remove(path)
+                            print(f"File {self.filename} deleted")
+                        except Exception as e:
+                            print(f"Error deleting file: {e}")
+                    else:
+                        print(f"file {filename} doesn't exist")
+
+            self.csv_file = None
+            self.csv_write = None
+
 
     def update_display(self, data_dict):
         """
         Handles data from PySensorFlow Engine
-        Received data_dict: {'ldr': 500, 'pot': 200}
+        Iterates through whatever data comes in and updates the matching plot.
         """
+
+        # dynamic plotting loop
+        for s_id, value in data_dict.items():
+            if s_id in self.curves:
+                #add to specific buffer
+                self.buffers[s_id].append(value)
+                #update specific curves
+                self.curves[s_id].setData(self.buffers[s_id])
+
         #get data
         ldr_val = data_dict.get('ldr_sensor', 0.0)
         pot_val = data_dict.get('threshold', 500.0)
+        temp_val = data_dict.get('temp_sensor', 300.0)
 
         #threshold control: manual or synced to hardware
         if self.chk_hw_sync.isChecked():
@@ -187,6 +264,7 @@ class ParticleDashboard(QMainWindow):
         #apply smoothing
         if self.chk_smooth.isChecked():
             self.smoothing_buffer.append(ldr_val)
+            self.smoothing_buffer_temp.append(temp_val)
             val_to_plot = sum(self.smoothing_buffer) / len(self.smoothing_buffer)
         else:
             val_to_plot = ldr_val
@@ -202,15 +280,7 @@ class ParticleDashboard(QMainWindow):
             self.graph_widget.setLabel('left', 'Raw ADC', units='0-1023')
             self.graph_widget.setYRange(0., 1024.)
     
-
-        #this function receives the data from signal
-        self.data_buffer.append(display_val)
-        if len(self.data_buffer) > 100:
-            self.data_buffer.pop(0)
-
-        self.data_line.setData(self.data_buffer)
-
-        if self.csv_write:
+        if self.csv_write and self.csv_file and not self.csv_file.closed:
             current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
             self.csv_write.writerow([current_time, display_val])
 
