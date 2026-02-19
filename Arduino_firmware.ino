b4dd64757a4d47ca5e95f5fd0b6dcbb47d711c06
Neeraj Amin <neeraj.amin94@gmail.com>
2026-02-19 23:57:14 +0100

    fix(firmare): sync arduino serial payload with setup.json sensor mapping

--- Arduino_firmware.ino
+++ Arduino_firmware.ino
@@ -1,5 +1,9 @@
-const int LDRPin = A0;
+const int ldrPin = A0;
+const int potPin = A1;
+const int temPin = A2;
+
 const int ledPin = 8;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -9,8 +13,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // set the brightness of pin 9:
-  int sensorValue = analogRead(LDRPin);
-  Serial.println(sensorValue);
+  int ldrValue = analogRead(ldrPin);
+  int potValue = analogRead(potPin);
+  int tempValue = analogRead(tempPin);
+
+  // transmitting sensor data as CSV
+  Serial.println(ldrValue);
+  Serial.println(",");
+  Serial.println(potValue);
+  Serial.println(",");
+  Serial.println(tempValue);
 
   if (Serial.available() > 0) {
     char command = Serial.read();
